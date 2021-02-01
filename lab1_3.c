869ba62b44e7f77e6d65810e2974479a3a34a3f4
6320502479 <pattrapol.J@ku.th>
2021-02-01 18:18:16 +0700

    Update lab1_3.c

--- lab1_3.c
+++ lab1_3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int r,mod3=0,mod11=0;
-    scanf("%d",&r);
-    if(r>=0 && r<=100000000)
+     long long int r,mod3=0,mod11=0;
+    scanf("%lld",&r);
+    if(r>=0 && r<=1000000000000)
     {
     mod3=r%3;
     mod11=r%11;
-    printf("%d %d",mod3,mod11);
+    printf("%lld %lld",mod3,mod11);
     }
     return 0;
 }

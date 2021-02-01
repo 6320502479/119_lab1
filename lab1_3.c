c62ff708386ac856408bf5bd2bad6881e7025ec1
6320502479 <pattrapol.J@ku.th>
2021-02-01 18:19:28 +0700

    Update lab1_3.c

--- lab1_3.c
+++ lab1_3.c
@@ -3,11 +3,8 @@ int main()
 {
      long long int r,mod3=0,mod11=0;
     scanf("%lld",&r);
-    if(r>=0 && r<=1000000000000)
-    {
     mod3=r%3;
     mod11=r%11;
     printf("%lld %lld",mod3,mod11);
-    }
     return 0;
 }

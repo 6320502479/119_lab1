02fa3dd8942855ac33f78206a25ccda510f4fbc9
6320502479 <pattrapol.J@ku.th>
2021-02-01 18:01:40 +0700

    Update lab1_3.c

--- lab1_3.c
+++ lab1_3.c
@@ -3,8 +3,11 @@ int main()
 {
     int r,mod3=0,mod11=0;
     scanf("%d",&r);
+    if(r>=0 && r<=100000000)
+    {
     mod3=r%3;
     mod11=r%11;
     printf("%d %d",mod3,mod11);
+    }
     return 0;
 }

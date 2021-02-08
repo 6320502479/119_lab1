832206ea2bf569019dedbb4200a764233b36c724
6320502479 <pattrapol.J@ku.th>
2021-02-08 17:58:19 +0700

    lab1

---
+++ lab1_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+ float time;
+ int min,l;
+  scanf("%f",&time);
+  l=floor(time);
+  min=(time-l)*100;
+   if(time>=12.00)
+   {
+   }
+   if(time<=11.59)
+   {
+   }
+   printf("%d",min);
+}
